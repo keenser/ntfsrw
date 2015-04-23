20c1e58041e3b765d44944c0e93dd0a0209d5752
German Skalauhov <keenser.sg@gmail.com>
2015-04-23 16:55:39 +0300

    Initial Commit

---
+++ ntfsrw/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ntfsrw
+//
+//  Created by German Skalauhov on 23.04.15.
+//  Copyright (c) 2015 German Skalauhov. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
